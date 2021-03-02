b36415102384cab8805bca21db08894f1d5df700
“Aaron <github email address>
2021-03-02 12:00:11 -0500

    stuck on 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // write your code here
+const ramenMenu = document.querySelector("div#ramen-menu")
+
+function renderAllRamens() {
+fetch ('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(ramenObj => {
+            renderOneNoodle(ramenObj)
+        })
+    })
+}
+
+function renderOneNoodle(ramenObj) {
+    const div = document.createElement('div')
+    div.dataset.id = ramenObj.id
+
+    div.innerHTML = `
+    <img src=${ramenObj.image} class="detail-image" />
+    `
+
+    ramenMenu.append(div)
+}
+
+ramenMenu.addEventListener('click', function (event){
+    const name = event.target.parentNode.dataset.id.name
+    const restaurant = event.target.parentNode.dataset.id.restaurant
+    const image = event.target.parentNode.dataset.id.image
+    fetch(`http://localhost:3000/ramens/${event.target.parentNode.dataset.id}`)
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    console.log(name)
+})
+
+
+renderAllRamens()
